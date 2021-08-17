d6c156b265a6ad0423f7580c6be6b850b3043f6c
panqinzhang <panqin.zhang@thoughtworks.com>
2021-08-17 20:54:40 +0800

    build:初始化测试

--- vars/serviceDeployPipeline.groovy
+++ vars/serviceDeployPipeline.groovy
@@ -4,5 +4,5 @@ def call(Map config) {
     def pipelineParam = new PipelineParam(config.serviceName)
     echo config.toString()
     echo pipelineParam.toString()
-//    serviceDeployMaster(new PipelineParam(config.serviceName))
+    serviceDeployMaster(pipelineParam)
 }
