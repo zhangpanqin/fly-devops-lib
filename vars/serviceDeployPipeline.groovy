ca796413dd4639eee182c74d387d4e8f941fff73
panqinzhang <panqin.zhang@thoughtworks.com>
2021-08-17 19:33:11 +0800

    build:初始化测试

--- vars/serviceDeployPipeline.groovy
+++ vars/serviceDeployPipeline.groovy
@@ -1,7 +1,5 @@
 import com.mflyyou.PipelineParam
 
 def call(Map config) {
-    if (env.BRANCH_NAME == 'master') {
-        serviceDeployMaster(new PipelineParam(config.serviceName))
-    }
+    serviceDeployMaster(new PipelineParam(config.serviceName))
 }
