7c6d5d6ea9e7eeb82858b9a8c8b52495152a7311
panqinzhang <panqin.zhang@thoughtworks.com>
2021-08-18 02:11:25 +0800

    build:初始化测试

--- vars/serviceDeployMaster.groovy
+++ vars/serviceDeployMaster.groovy
@@ -9,6 +9,7 @@ import com.mflyyou.ServicePipelineHelper
  * env.BUILD_URL
  */
 def call(PipelineParam config) {
+    echo env.BRANCH_NAME
     def servicePipelineHelper = new ServicePipelineHelper(this, config.getServiceName(), env.BRANCH_NAME)
     def gitHelper = new GitHelper(this)
     pipeline {
