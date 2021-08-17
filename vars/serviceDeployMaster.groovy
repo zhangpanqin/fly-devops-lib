4639405646a205a736a0b21271c1f63063f2ef4c
panqinzhang <panqin.zhang@thoughtworks.com>
2021-08-18 00:47:24 +0800

    build:初始化测试

--- vars/serviceDeployMaster.groovy
+++ vars/serviceDeployMaster.groovy
@@ -33,7 +33,7 @@ def call(PipelineParam config) {
                 steps {
                     script {
                         echo config.toString()
-                        echo "env"
+                        echo ${env}
                     }
                     withAWS(credentials: 'aws-iam-fly-devops', region: 'us-east-2') {
                         sh 'aws iam get-user'
