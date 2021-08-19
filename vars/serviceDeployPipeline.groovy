56821ccfd3fad7b13636ffe90f71892fcbb21f68
panqinzhang <panqin.zhang@thoughtworks.com>
2021-08-19 23:20:57 +0800

    build:初始化测试

--- vars/serviceDeployPipeline.groovy
+++ vars/serviceDeployPipeline.groovy
@@ -1,15 +1,6 @@
 import com.mflyyou.PipelineParam
 
 def call(Map config) {
-    node {
-        sh 'git rev-parse --abbrev-ref HEAD > GIT_BRANCH'
-        git_branch = readFile('GIT_BRANCH').trim()
-        echo git_branch
-
-        sh 'git rev-parse HEAD > GIT_COMMIT'
-        git_commit = readFile('GIT_COMMIT').trim()
-        echo git_commit
-    }
     echo "serviceDeployPipeline.groovy echo ${env.GIT_BRANCH}"
     def pipelineParam = new PipelineParam(config.serviceName, "${env.GIT_BRANCH}")
     serviceDeployMaster(pipelineParam)
