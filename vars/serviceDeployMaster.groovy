24bd3be24bbf5562c20cddbab2e05b8129c99717
panqinzhang <panqin.zhang@thoughtworks.com>
2021-08-20 03:50:33 +0800

    build:初始化测试

--- vars/serviceDeployMaster.groovy
+++ vars/serviceDeployMaster.groovy
@@ -42,8 +42,10 @@ def call(PipelineParam config) {
 //                        def images = ecrListImages(repositoryName: "${serviceName}", filter: "imageTag=HEAD-24b17ec")
 //                    }
                     script {
-                        def data = sh(returnStdout: true, script: "aws ecr describe-images --region us-east-2 --repository-name=${serviceName} --image-ids=imageTag=HEAD-24b17ec")
-                        echo "${data}"
+                        withAWS(credentials: 'aws-iam-fly-devops', region: 'us-east-2') {
+                            def data = sh(returnStdout: true, script: "aws ecr describe-images --region us-east-2 --repository-name=${serviceName} --image-ids=imageTag=HEAD-24b17ec")
+                            echo "${data}"
+                        }
                         if (fileExists("/git-2.33.0")) {
                             echo "git-2.33.0 exist"
                         } else {
