c2f837ce31c8c52b040d7efa9f968efa3db120e5
panqinzhang <panqin.zhang@thoughtworks.com>
2021-08-19 22:48:22 +0800

    build:初始化测试

--- vars/serviceDeployPipeline.groovy
+++ vars/serviceDeployPipeline.groovy
@@ -1,7 +1,7 @@
 import com.mflyyou.PipelineParam
 
 def call(Map config) {
-    echo "serviceDeployPipeline.groovy echo ${env.GIT_BRANCH}"
-    def pipelineParam = new PipelineParam(config.serviceName,"${env.GIT_BRANCH}")
+    echo "serviceDeployPipeline.groovy echo ${env.BRANCH_NAME}"
+    def pipelineParam = new PipelineParam(config.serviceName,"${env.BRANCH_NAME}")
     serviceDeployMaster(pipelineParam)
 }
